16b9870138c65f855d0abfe0e7b01c915ca91509
GusBTB <gustavocs81@gmail.com>
2023-02-14 16:41:13 -0300

    :hammer: adding cors to app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,11 @@ import express from "express";
 import { routes } from "./routes";
 import errorMiddleWare from "./middlewares/error.middleware";
 
+let cors = require("cors");
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 routes(app);
 app.use(errorMiddleWare);
