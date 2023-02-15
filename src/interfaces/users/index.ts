ffaa1c2c568f7f4f74eb0504837b26e230240ec3
LeandroJunges <leandrojunges21@gmail.com>
2023-02-15 15:23:49 -0300

    :sparkles: created route to edit address

--- src/interfaces/users/index.ts
+++ src/interfaces/users/index.ts
@@ -1,16 +1,26 @@
 export interface IUserRequest {
-  name: string;
-  email: string;
-  cpf: string;
-  cellphone: string;
-  password: string;
-  description: string;
-  dateOfBirth: Date;
-  isAdvertiser: boolean;
-  cep: string;
-  state: string;
-  city: string;
-  street: string;
-  number: string;
-  complement?: string;
+  name          : string;
+  email         : string;
+  cpf           : string;
+  cellphone     : string;
+  password      : string;
+  description   : string;
+  dateOfBirth   : Date;
+  isAdvertiser  : boolean;
+  cep           : string;
+  state         : string;
+  city          : string;
+  street        : string;
+  number        : string;
+  complement   ?: string;
+}
+
+
+export interface IAddressRequest{
+    cep       : string;
+    state     : string;
+    city      : string;
+    number    : string;
+    complement: string;
+    street    : string;
 }
---
+++ src/services/address/editAddress.services.ts
@@ -0,0 +1,47 @@
+import { IAddressRequest } from './../../interfaces/users/index';
+import AppDataSource from "../../data-source"
+import { Address } from "../../entities/Address.entity"
+import { User } from "../../entities/User.entity"
+import { AppError } from '../../errors/appError';
+
+const editAddressService = async ({cep,city,complement,number,state, street}:IAddressRequest, id:string)=>{
+
+    
+    const userRepository = AppDataSource.getRepository(User)
+    const addressRepository = AppDataSource.getRepository(Address)
+
+    // const findAddress = addressRepository.findOne({
+    //     where: {
+
+    // })
+
+    const findUser = await userRepository.findOneBy({
+        id
+    })
+
+    const addressId = findUser?.address.id
+
+    if(!findUser){
+        throw new AppError(404, "User not found!")
+    }
+
+    
+    
+
+    await addressRepository.update(addressId!,{
+        cep: cep? cep : findUser.address.cep,
+        city: city? city : findUser.address.city,
+        complement: complement? complement : findUser.address.complement,
+        number: number? number : findUser.address.number,
+        state: state? state : findUser.address.state,
+        street : street? street : findUser.address.street
+    })
+    const user = await userRepository.findOneBy({
+        id
+    })
+
+    return user
+
+}
+
+export default editAddressService
